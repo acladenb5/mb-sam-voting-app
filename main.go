8e7a09921dd3027537be989a492e3f845dc8a2e3
cladena <cladena@amazon.com>
2019-02-05 14:52:33 -0500

    ResultHandler fixed

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 
@@ -77,15 +76,10 @@ func ResultsHandler(w http.ResponseWriter, r *http.Request) {
 	TotalP1 = 5403
 	TotalP2 = 6603
 	TotalP3 = 10
-	ResP1 := Results{Pid: "Participant 1", Result: TotalP1}
-	ResP2 := Results{Pid: "Participant 2", Result: TotalP2}
-	ResP3 := Results{Pid: "Participant 3", Result: TotalP3}
 	var RetResults []Results
-	RetResults[0] = ResP1
-	RetResults[1] = ResP2
-	RetResults[3] = ResP3
-	// RetResults := Results{TotalP1, TotalP2, TotalP3}
-	fmt.Println(RetResults)
+	RetResults = append(RetResults, Results{"Participant 1", TotalP1})
+	RetResults = append(RetResults, Results{"Participant 2", TotalP2})
+	RetResults = append(RetResults, Results{"Participant 3", TotalP3})
 	json.NewEncoder(w).Encode(RetResults)
 }
 
